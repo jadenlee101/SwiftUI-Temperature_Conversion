4fa788906262574f03e9d46bd381d80a919c30f9
Jaden Lee <jaden.lee101@gmail.com>
2023-05-31 11:21:19 -0600

    Initial Commit

---
+++ Temperature Conversion/Temperature_ConversionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Temperature_ConversionApp.swift
+//  Temperature Conversion
+//
+//  Created by Jaden Lee on 2023-05-31.
+//
+
+import SwiftUI
+
+@main
+struct Temperature_ConversionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
